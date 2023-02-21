f66669051dad03e5b47c8d73e27698f8fe00cb26
AyrtonRZ <ayrtonrodrigues45@gmail.com>
2023-02-21 12:18:28 -0300

    adjust: ajustando

--- PHP-POO-avancando/projeto/src/Pessoa.php
+++ PHP-POO-avancando/projeto/src/Pessoa.php
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-<?php 
-
-class Pessoa
-{
-    public string $nome;
-    public Cpf $cpf;
-
-    public function __construct(string $nome, Cpf $cpf)
-    {
-=======
 <?php
 
 class Pessoa
@@ -16,44 +5,23 @@ class Pessoa
     protected string $nome;
     private Cpf $cpf;
 
-
     public function __construct(string $nome, Cpf $cpf)
     {
         $this->validaNome($nome);
->>>>>>> 83e9613795de0e89b216b49a05611a2936a1d597
         $this->nome = $nome;
         $this->cpf = $cpf;
     }
 
-<<<<<<< HEAD
-    public function retornaNome():string
-=======
     public function retornaNome(): string
->>>>>>> 83e9613795de0e89b216b49a05611a2936a1d597
     {
         return $this->nome;
     }
 
-<<<<<<< HEAD
-    public function retornaCpf():string
-=======
     public function retornaCpf(): string
->>>>>>> 83e9613795de0e89b216b49a05611a2936a1d597
     {
         return $this->cpf->retornaNumero();
     }
 
-<<<<<<< HEAD
-    public function validaNomeTitular(string $nomeTitular)
-    {
-        if(strlen($nomeTitular) < 5){
-        echo "Nome precisa de no minimo 5 caracteres.";
-            exit();
-        }
-    }
-
-}
-=======
     protected function validaNome(string $nome)
     {
         if (strlen($nome) < 5) {
@@ -62,4 +30,3 @@ class Pessoa
         }
     }
 }
->>>>>>> 83e9613795de0e89b216b49a05611a2936a1d597
